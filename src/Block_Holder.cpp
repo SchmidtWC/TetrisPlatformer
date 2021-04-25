06b43e9ac9d945b96b338cdb081e47ac9c05475e
PierceRTarleton <72158957+PierceRTarleton@users.noreply.github.com>
2021-04-25 19:00:56 -0400

    Updated

--- src/Block_Holder.cpp
+++ src/Block_Holder.cpp
@@ -1,9 +1,9 @@
 7#include "../include/Block_Holder.h"
 
 
-void Block_Holder::Rotate(){
-	Blocks[0].rotate();
-	Blocks[1].rotate();
+void Block_Holder::Rotate(bool direct){
+	Blocks[0].rotate(direct);
+	Blocks[1].rotate(direct);
 	Hit_Boxes[0] = Blocks[0].Box;
 	Hit_Boxes[1] = Blocks[1].Box;
 }
