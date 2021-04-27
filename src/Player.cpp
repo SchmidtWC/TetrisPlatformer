109f6b3b63c4f897063805a465886babe795811b
PierceRTarleton <72158957+PierceRTarleton@users.noreply.github.com>
2021-04-27 11:28:53 -0400

    Fixed wall sticking, floor clipping I think

--- src/Player.cpp
+++ src/Player.cpp
@@ -5,15 +5,24 @@ void Player::controller(SDL_Event& input){
     if (input.type == SDL_KEYDOWN) {
 		switch (input.key.keysym.sym) {
 		case SDLK_a:
-			Run_L();
+			if (runLstop) {
+				runLstop = false;
+				Run_L();
+			}
 			break;
 
 		case SDLK_d:
-			Run_R();
+			if (runRstop) {
+				runRstop = false;
+				Run_R();
+			}
 			break;
 
 		case SDLK_SPACE:
-			jump();
+			if (end_jump) {
+				end_jump = false;
+				jump();
+			}
 			break;
 		case SDLK_LSHIFT:
 			Magic = true;
@@ -22,15 +31,20 @@ void Player::controller(SDL_Event& input){
 	if (input.type == SDL_KEYUP) {
 		switch (input.key.keysym.sym) {
 		case SDLK_a:
+			runLstop = true;
 			if (velX < 0){
 				stop_run();
 			}
 			break;
 
 		case SDLK_d:
+			runRstop = true;
 			if (velX > 0) {
 				stop_run();
 			}
+			break;
+		case SDLK_SPACE:
+			end_jump = true;
 			break;
 		case SDLK_LSHIFT:
 			Magic = false;
@@ -54,20 +68,20 @@ void Player::updatePos() {
 void Player::jump() {
 	// Sets Vertical velocity, and Player state to Jump
 	if (Jump_Frame == 0 && state != JUMP) {
-		std::cout << "j" << std::endl;
-		Jump_Start = SDL_GetTicks()/ Jump_FR;
+		std::cout << Hit_Boxes[0].BE << std::endl;
+		Jump_Start = SDL_GetTicks() / Jump_FR;
 		velY = -5;
 		state = JUMP;
 	}
-	
+
 	/*Increases the Y velocity when Jump_Frame modulus 7 = 0,
 	  and the Y velocity is less then or equal to 4*/
-	else if (Jump_Frame < (SDL_GetTicks() / Jump_FR - Jump_Start) && velY <=4) {
+	else if (Jump_Frame < (SDL_GetTicks() / Jump_FR - Jump_Start) && velY <= 4) {
 		Jump_Frame = (SDL_GetTicks() / Jump_FR - Jump_Start);
 		velY += Gravity;
-		if (velY == 0) {
+		/*if (velY == 0) {
 			Gravity = 2;
-		}
+		}*/
 	}
 
 	//Adjust Player position and boundaries based on the X and Y velocity
@@ -78,17 +92,17 @@ void Player::jump() {
 	Hit_Boxes[0].LE = destRect.x;
 	Hit_Boxes[0].RE = destRect.x + destRect.w;
 	Hit_Boxes[1].BE = Hit_Boxes[0].BE - KickH;
-    Hit_Boxes[1].TE = Hit_Boxes[1].BE - KickH;
+	Hit_Boxes[1].TE = Hit_Boxes[1].BE - KickH;
 	Hit_Boxes[1].LE = Hit_Boxes[0].LE - KickL;
 	Hit_Boxes[1].RE = Hit_Boxes[0].LE;
 	Hit_Boxes[2].LE = Hit_Boxes[0].RE;
 	Hit_Boxes[2].RE = Hit_Boxes[1].RE + KickL;
 	Hit_Boxes[2].BE = Hit_Boxes[0].BE - KickH;
-    Hit_Boxes[2].TE = Hit_Boxes[2].BE - KickH;
+	Hit_Boxes[2].TE = Hit_Boxes[2].BE - KickH;
 }
 
 void Player::stop_jump() {
-	Gravity = 1;
+	//Gravity = 1;
 
 	//else if the player Velocity X = 0 Set state = Idle
 	if (velX == 0) {
@@ -111,7 +125,6 @@ void Player::Run_R() {
 		state = RUN;
 	velX = 4;
 	left = 0;
-
 }
 
 
@@ -126,26 +139,32 @@ void Player::Run_L() {
 
 /*Update Player position based on Player X velocity*/
 void Player::run() {
-	destRect.y += velY;
+	//destRect.y += velY;
 	destRect.x += velX;
-	Hit_Boxes[0].TE = destRect.y;
-	Hit_Boxes[0].BE = destRect.y + destRect.h;
+	//Hit_Boxes[0].TE = destRect.y;
+	//Hit_Boxes[0].BE = destRect.y + destRect.h;
 	Hit_Boxes[0].LE = destRect.x;
 	Hit_Boxes[0].RE = destRect.x + destRect.w;
-	Hit_Boxes[1].BE = Hit_Boxes[0].BE - KickH;
-    Hit_Boxes[1].TE = Hit_Boxes[1].BE - KickH;
+	//Hit_Boxes[1].BE = Hit_Boxes[0].BE - KickH;
+	//Hit_Boxes[1].TE = Hit_Boxes[1].BE - KickH;
 	Hit_Boxes[1].LE = Hit_Boxes[0].LE - KickL;
 	Hit_Boxes[1].RE = Hit_Boxes[0].LE;
 	Hit_Boxes[2].LE = Hit_Boxes[0].RE;
 	Hit_Boxes[2].RE = Hit_Boxes[1].RE + KickL;
-	Hit_Boxes[2].BE = Hit_Boxes[0].BE - KickH;
-    Hit_Boxes[2].TE = Hit_Boxes[2].BE - KickH;
+	//Hit_Boxes[2].BE = Hit_Boxes[0].BE - KickH;
+	//Hit_Boxes[2].TE = Hit_Boxes[2].BE - KickH;
 
 }
 
 void Player::stop_run() {
 	velX = 0;
-	state = JUMP;
+	//runstop = true;
+	if (velY != 0) {
+		state = JUMP;
+	}
+	else {
+		state = IDLE;
+	}
 }
 
 Hit_Box Player::get_Hitbox() {
@@ -157,27 +176,38 @@ Hit_Box Player::Kick(){
 }
 
 void Player::collision_response(char type, int edge, int Obj_index) {
-	//std::cout << type << std::endl;
+	std::cout << state << std::endl;
+	bool collision = true;
 	switch (type) {
             case 'B':
-				velY = 1;
+				velY = 0;
+				if (edge > Hit_Boxes[0].BE) {
+					collision = false;
+				}
+				//std::cout << edge << std::endl;
+				//std::cout << Hit_Boxes[0].LE << std::endl;
+				//std::cout << Hit_Boxes[0].BE << std::endl;
 				destRect.y -= Hit_Boxes[0].BE - edge;
 				Rotate_block = Obj_index;
 				Hit_Boxes[0].TE = destRect.y;
 				Hit_Boxes[0].BE = destRect.y + destRect.h;
-                stop_jump();
+				//if (collision) {
+				stop_jump();
+				//}
                 break;
             case 'R':
 				velX = 0;
-				destRect.x += edge - Hit_Boxes[0].RE - 2;
+				destRect.x += edge - Hit_Boxes[0].RE - 1;
 				Hit_Boxes[0].LE = destRect.x;
 				Hit_Boxes[0].RE = destRect.x + destRect.w;
+				//stop_run();
                 break;
             case 'L':
 				velX = 0;
-				destRect.x -= Hit_Boxes[0].LE - edge - 2;
+				destRect.x -= Hit_Boxes[0].LE - edge - 1;
 				Hit_Boxes[0].LE = destRect.x;
 				Hit_Boxes[0].RE = destRect.x + destRect.w;
+				//stop_run();
                 break;
             case 'T':
 				velY = 0;
